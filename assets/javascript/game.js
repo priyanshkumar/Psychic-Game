388dc0d8a0f916b6d10d204d0412ae6a20b4c729
Priyansh <pradadiya667@gmail.com>
2019-02-19 20:01:41 -0500

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,27 +33,62 @@ var counter = 0;
 var loss = 0;
 var guess = 10;
 
+var random = Math.floor(Math.random() * 26 + 1);
+
 document.onkeyup = function(event) {
   var guessOutput = document.getElementById("guessLeft");
   var letterOutput = document.getElementById("letterGuess");
   var lossOutput = document.getElementById("lossText");
   var winOutput = document.getElementById("winText");
+  var guessletter = document.getElementById("guess");
+  var messageOutput = document.getElementById("message");
+
   winOutput.textContent = wins;
   guessOutput.textContent = guess;
   lossOutput.textContent = loss;
+  guessletter.textContent = "_";
+
   userenter[counter] = event.key;
 
-  var random = Math.floor(Math.random() * 26 + 1);
+  messageOutput.textContent = "";
+
+  if (
+    guessletter.classList.contains("green") ||
+    messageOutput.classList.contains("green")
+  ) {
+    guessletter.classList.remove("green");
+    messageOutput.classList.remove("green");
+  }
+  if (
+    guessletter.classList.contains("red") ||
+    messageOutput.classList.contains("red")
+  ) {
+    guessletter.classList.remove("red");
+    messageOutput.classList.remove("red");
+  }
+
   if (counter <= 8) {
     if (userenter[counter] === letterArray[random]) {
       wins++;
       counter = 0;
       guess = 10;
+
+      guessletter.classList.add("green");
+      messageOutput.classList.add("green");
+
+      guessletter.textContent = letterArray[random];
+      letterOutput.textContent = letters;
       winOutput.textContent = wins;
-      alert("You guessed letter '" + letterArray[random] + "' right!");
+      guessOutput.textContent = guess;
+      messageOutput.textContent =
+        "You guessed letter '" + letterArray[random] + "' right!";
+
+      userenter.length = 0;
+      random = Math.floor(Math.random() * 26 + 1);
     } else {
       counter++;
       guess--;
+
       guessOutput.textContent = guess;
       for (var i = 0; i < userenter.length; i++) {
         var letters = letters + "," + userenter[i];
@@ -64,9 +99,19 @@ document.onkeyup = function(event) {
     loss++;
     counter = 0;
     guess = 10;
+
+    guessletter.classList.add("red");
+    messageOutput.classList.add("red");
+
+    guessletter.textContent = letterArray[random];
     lossOutput.textContent = loss;
+    guessOutput.textContent = guess;
     letterOutput.textContent = " ";
+    letterOutput.textContent = letters;
+    messageOutput.textContent =
+      "You were unabel to guess letter '" + letterArray[random] + "'";
+
     userenter.length = 0;
-    alert("You were unabel to guess letter '" + letterArray[random] + "'");
+    random = Math.floor(Math.random() * 26 + 1);
   }
 };
